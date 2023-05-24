66d6e0714dbfc8046b65740f3b2c4ad0161f548a
Robin Martinsen <72741881+lDenzh@users.noreply.github.com>
2023-05-24 14:33:26 +0200

    removed unnecessary imports +

--- vite-react-frontend/src/main.tsx
+++ vite-react-frontend/src/main.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import FileUpload from './App'
 import App from './App'
 import './index.css'
 
-import PDFevaluate from './PDFevaluate'
-import Layout from './Layout'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
